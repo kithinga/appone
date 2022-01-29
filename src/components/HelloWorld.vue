c62c643e5c82bbebfc56c2bb5369efc2a558f990
Festo <menscfesz@gmail.com>
2022-01-29 20:43:08 +0300

    jj

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,6 +2,13 @@
   <div class="hello">
     <h5>Hello Kithinga, you are great</h5>
     <p>This is another way to serve</p>
+
+    <form action="">
+      <button class="one">Colours</button>
+      <button class="two">Ideaas</button>
+      <button class="three">Artkit</button>
+      <button class="four">Websites</button>
+    </form>
   </div>
 </template>
 
@@ -29,5 +36,45 @@ li {
 }
 a {
   color: #42b983;
+}
+.one{
+  border: none;
+  height:20rem;
+  border-radius:5px;
+  background-color:cyan;
+  width: 20rem;
+  font-size:2.1rem;
+  font-weight:500;
+  margin:1.2rem;
+}
+.two{
+  border: none;
+  height:20rem;
+  border-radius:5px;
+  background-color:rgb(217, 255, 0);
+  width: 20rem;
+  font-size:2.1rem;
+  font-weight:500;
+  margin:1.2rem;
+}
+.three{
+  border: none;
+  height:20rem;
+  border-radius:5px;
+  background-color:rgb(187, 77, 13);
+  width: 20rem;
+  font-size:2.1rem;
+  font-weight:500;
+  margin:1.2rem;
+}
+.four{
+  border: none;
+  height:20rem;
+  border-radius:5px;
+  background-color:rgb(227, 238, 238);
+  width: 20rem;
+  font-size:1.2rem;
+  font-weight:520;
+  margin:1.2rem;
 }
 </style>
